c5c7a796c4337f70cac3841939895110acdc8157
miiin_sseong <wsc03002@naver.com>
2022-05-19 22:36:33 +0900

    선언적 렌더링과 입력 핸들링

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1 @click="increase" >
+    {{count}}
+  </h1>
+</template>
+
+<script>
+export default{
+data() {
+  return {
+    count: 0
+  }
+},
+  methods: {
+    // app.vue안에 있는 script를 지칭하는 this
+    increase() {
+      this.count += 1
+    }
+  },
+}
+</script>
+
+<style>
+h1{
+  font-size: 50px;
+  color: royalblue;
+  font-weight: 500;
+}
+</style>
